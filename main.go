daf8fdc84b982077b97540d6042d55ab535db85b
Denis Budyak <budden73@gmail.com>
2019-03-30 16:13:28 +0300

    Tuning of the server

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/budden/rqr/pkg/errorcodes"
+	"golang.org/x/net/netutil"
 )
 
 const (
@@ -20,7 +23,25 @@ func main() {
 	http.HandleFunc("/fetchtasklist", handleFetchTaskList)
 	http.HandleFunc(fetchTaskGetURL, handleFetchTaskGet)
 	http.HandleFunc(fetchTaskDeleteURL, handleFetchTaskDelete)
-	log.Fatal(http.ListenAndServe(":8086", nil))
+
+	// https://habr.com/ru/post/197468/
+	ThisHTTPServer := &http.Server{
+		Addr:           ":8086",
+		Handler:        http.DefaultServeMux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20}
+
+	listener, err := net.Listen("tcp", ThisHTTPServer.Addr)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	limitListener := netutil.LimitListener(listener, 10)
+
+	log.Fatalln(ThisHTTPServer.Serve(limitListener))
+
 }
 
 func handleRoot(w http.ResponseWriter, req *http.Request) {
